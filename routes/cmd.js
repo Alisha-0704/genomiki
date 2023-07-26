006de64c5f8ec9450c910ed38b2515be49281727
Alisha-0704 <70397438+Alisha-0704@users.noreply.github.com>
2023-07-26 15:27:41 +0530

    Script showing current date runs successfully.

--- routes/cmd.js
+++ routes/cmd.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const { exec } = require('child_process');
 
 router.get('/', (req, res) => {
-     // Set the PATH environment variable to include the necessary locations
-  const newPath = process.env.PATH + ':"C:/Users/alish/Downloads/v0.12.1/s-andrews-FastQC-e7ef390/fastqc"';
-  process.env.PATH = newPath;
+
   // Run the test.sh script using child_process.exec()
-  exec('bash C:/Users/alish/Downloads/genomiki/public/test.sh', { shell: 'C:/Program Files/Git/bin/bash.exe' }, (error, stdout, stderr) => {
+  exec('bash C:/Users/alish/Downloads/genomiki/public/test.sh', (error, stdout, stderr) => {
     if (error) {
       console.error('Error executing test.sh:', error);
       res.status(500).send('Error executing test.sh');
